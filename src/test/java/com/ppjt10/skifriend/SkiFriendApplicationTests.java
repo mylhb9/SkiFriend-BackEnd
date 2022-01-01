ad45e3451a58e90f3c1080998cbe075a5e71779b
mylhb9 <mylhb9@naver.com>
2022-01-01 21:29:29 +0900

    ff

--- src/test/java/com/ppjt10/skifriend/SkiFriendApplicationTests.java
+++ src/test/java/com/ppjt10/skifriend/SkiFriendApplicationTests.java
@@ -1,13 +1,13 @@
-package com.ppjt10.skifriend;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-//@SpringBootTest
-class SkiFriendApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
-
-}
+//package com.ppjt10.skifriend;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+////@SpringBootTest
+//class SkiFriendApplicationTests {
+//
+//    @Test
+//    void contextLoads() {
+//    }
+//
+//}
